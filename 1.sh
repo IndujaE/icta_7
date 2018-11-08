b2767f8a7f170698158e23f33e56bfec07afcf91
saifcool <saifshereef786@gmail.com>
2018-11-08 16:38:57 +0530

    day2

---
+++ 1.sh
@@ -0,0 +1,7 @@
+echo "# icta_7" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/IndujaE/icta_7.git
+git push -u origin master
+echo "end"
